47720e7fc8e6f10b3e7952bfcf47920b25572260
Dudkin Sergey <dsergik@mail.ru>
2023-08-06 12:22:13 +0300

    new project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Layout/Header/Header';
+import Main from './Pages/Main';
+import Footer from './Layout/Footer/Footer';
+import './App.scss';
+import Navigation from './Layout/Navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <Header />
+      <Footer />
+    
     </div>
   );
 }
