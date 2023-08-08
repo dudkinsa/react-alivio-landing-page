cbffe0859ce42c4226d4475564541fd803d5ffd2
Dudkin Sergey <dsergik@mail.ru>
2023-08-08 05:55:34 +0300

    Create page Home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import '../Home/home.scss'
+import '../Home/home.scss';
+import homeBg from '../../assets/img/home-bg.jpg'
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <div className="container">
+        <div className="home__content">
+          <h1 className="home__title"> Be aware, Manage well.</h1>
+          <div className="home__text">
+            <p>
+              Everyone experiences stress in different ways. Let Alivio guide
+              you, in a personalized journal experience, to overcome your
+              stress.
+            </p>
+          </div>
+          <div className="home__btn">
+            <a className='btn btn-big' href="#!">Find Your Way</a>
+          </div>
+        </div>
+      </div>
+      <div className="home__img-big">
+        <img src= {homeBg} alt="Girl" />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
